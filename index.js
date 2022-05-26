31f2bc2cea95787a8f8fa5f50136bb839113d5f0
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-05-26 23:14:01 +0600

    problem

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World Mosiur islam!')
+    res.send('Hello World')
 })
 
 app.listen(port, () => {
